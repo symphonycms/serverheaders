cbcbea945afec2bffac7396f1e7f62b970eb8e3d
Remie Bolte <r.bolte@gmail.com>
2012-08-28 08:08:46 +0200

    Remove support for pre-symphony-2.3 Use config group with list of included headers rather than hardcoded list

--- extension.driver.php
+++ extension.driver.php
@@ -2,15 +2,17 @@
 
 	Class extension_ServerHeaders extends Extension{
 	
-		public function about(){
-			return array('name' => 'Data Source: Server Headers',
-						 'version' => '1.2',
-						 'release-date' => '2011-03-22',
-						 'author' => array('name' => 'Symphony Team',
-										   'website' => 'http://symphony-cms.com'),
-						 'description' => ''
-				 	);
+		public function install() {
+			if(Symphony::Configuration()->get('include', 'server-headers') == '') {
+				$includes = 'HTTP_REFERER,HTTP_USER_AGENT,HTTP_HOST,SERVER_NAME,SERVER_ADDR,SERVER_PORT,REMOTE_ADDR,QUERY_STRING,HTTP_X_FORWARDED_FOR';
+				Symphony::Configuration()->set('include', $includes, 'server-headers');
+				Symphony::Configuration()->write();
+			}
 		}
-				
+
+		public function uninstall() {
+			Symphony::Configuration()->remove('server-headers');
+			Symphony::Configuration()->write();
+		}
 	}
 	
